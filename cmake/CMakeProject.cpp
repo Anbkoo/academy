bd5ed57509becad83377150be792b87b9e231b28
Anna Biloha <anna.biloha@materialise.kiev.ua>
2021-08-04 17:29:58 +0300

    cmake

---
+++ cmake/CMakeProject.cpp
@@ -0,0 +1,11 @@
+﻿// CMakeProject.cpp : Defines the entry point for the application.
+//
+#include <iostream>
+#include "myLibrary.h"
+
+int main()
+{
+	printHello();
+	std::cin.get();
+	return 0;
+}
---
+++ cmake/MyLibrary/myLibrary.cpp
@@ -0,0 +1,6 @@
+#include "myLibrary.h"
+
+void printHello()
+	{
+	std::cout << "Hello CMake_2" << std::endl;
+	}
